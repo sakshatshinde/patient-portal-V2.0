6d7a19fb2de533f42b4501df1adf99bd5bbdb8da
Sakshat Shinde <sakshatshinde@gmail.com>
2019-02-20 20:37:28 +0530

    Added routing & basic templates

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id' : 1,
+            'title' : 'Article One',
+            'patient'  : 'lorem',
+            'doctor' : 'ipsum',
+            'created_date' : '20-02-2019'
+        },
+        {
+            'id' : 2,
+            'title' : 'Article tow',
+            'patient'  : 'lorem',
+            'doctor' : 'chintu',
+            'created_date' : '20-02-2019'
+        },
+        {
+            'id' : 1,
+            'title' : 'Article three',
+            'patient'  : 'lorem',
+            'doctor' : 'babu',
+            'created_date' : '20-02-2019'
+        }
+    ]
+    return articles
