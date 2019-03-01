73b62e53eb6fdd04b6aab8010f10b8866d964b3e
Sakshat Shinde <sakshatshinde@gmail.com>
2019-03-01 12:30:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, flash, redirect, url_form, session, logging
 from data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 
@@ -22,5 +25,7 @@ def articles():
 @app.route('/article/<string:id>')
 def article(id):
     return render_template('article.html', id = id)
+    
+    
 if(__name__ == '__main__'):
     app.run(debug = True)
