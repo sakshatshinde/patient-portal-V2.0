21743266b3330b916ee4813f9c7800c12835e13d
Sakshat Shinde <sakshatshinde@gmail.com>
2019-03-01 12:48:09 +0530

    Adding /register

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template, flash, redirect, url_form, session, logging
+from flask import Flask , request, render_template, flash, redirect, url_for, session, logging
 from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
@@ -25,7 +25,30 @@ def articles():
 @app.route('/article/<string:id>')
 def article(id):
     return render_template('article.html', id = id)
-    
-    
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min = 1, max = 50)])
+    username = StringField('Username', [validators.Length(min=4, max = 25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Wrong passowrd')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if(request.method == 'POST' and form.validate()):
+        return render_template('register.html', form = form)
+    return render_template('register.html', form = form)
+
+
+
+
+
+
+#APP    
 if(__name__ == '__main__'):
     app.run(debug = True)
