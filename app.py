430aa5b3ab96ea24054839664fcfa126f75032b1
Sakshat Shinde <sakshatshinde@gmail.com>
2019-03-04 21:10:30 +0530

    added icons

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , request, render_template, flash, redirect, url_for, session, logging
+from flask import Flask , request, render_template, flash, redirect, url_for, session, logging, Markup
 #from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
